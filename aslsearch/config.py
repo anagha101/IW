bee7393082b83158b8ab676b52d9d3a290bfec09
Anagha Rajagopalan <68399871+anagha101@users.noreply.github.com>
2022-03-25 01:55:59 -0400

    goin back

--- aslsearch/config.py
+++ aslsearch/config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = 'asl_search_iw_app'
     # SQLALCHEMY_DATABASE_URI ='sqlite:///site.db'
     if os.environ.get('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
         # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
         # SQLALCHEMY_DATABASE_URI = 'postgres://zxcztxnsiaqmcs:bba59de47e28686189bbff3accfaa503cf44e979cb27c1f8a3b5e8d2bc8dbeff@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d5c55gh1c6l45j'
     else:
