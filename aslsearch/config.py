5ae51d0612329cd35fdf2c7884edc72d044118d1
Anagha Rajagopalan <68399871+anagha101@users.noreply.github.com>
2022-03-24 20:31:54 -0400

    heroku db setup

--- aslsearch/config.py
+++ aslsearch/config.py
@@ -5,7 +5,11 @@ password = os.environ.get('DB_PASSWORD')
 class Config:
     SECRET_KEY = 'asl_search_iw_app'
     # SQLALCHEMY_DATABASE_URI ='sqlite:///site.db'
-    SQLALCHEMY_DATABASE_URI = f'postgresql://zxcztxnsiaqmcs:bba59de47e28686189bbff3accfaa503cf44e979cb27c1f8a3b5e8d2bc8dbeff@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d5c55gh1c6l45j'
+    if os.environ.get('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+        # SQLALCHEMY_DATABASE_URI = 'postgres://zxcztxnsiaqmcs:bba59de47e28686189bbff3accfaa503cf44e979cb27c1f8a3b5e8d2bc8dbeff@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d5c55gh1c6l45j'
+    else:
+        SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:{password}@localhost/aslsearch'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = 'development'
