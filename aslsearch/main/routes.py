c959de15cb30758ebe142c785ff898ac39feee27
Anagha Rajagopalan <68399871+anagha101@users.noreply.github.com>
2022-04-05 00:51:42 -0400

    working on upload - interim stage

--- aslsearch/main/routes.py
+++ aslsearch/main/routes.py
@@ -7,12 +7,17 @@ from wtforms import StringField
 from wtforms import SubmitField
 from wtforms.validators import DataRequired
 from wtforms.validators import URL
+import sqlalchemy
 
 class WordForm(FlaskForm):
     word = StringField('Word', validators=[DataRequired()])
     submit = SubmitField('Add Word')
 
 class DefinitionForm(FlaskForm):
+    definition = StringField('Definition', validators=[DataRequired()])
+    submit = SubmitField('Add Definition')
+
+class EditDefinitionForm(FlaskForm):
     definition = StringField('Definition', validators=[DataRequired()])
     submit = SubmitField('Add Definition')
 
@@ -59,9 +64,9 @@ def words():
 @main.route("/wordpage/<string:title>", methods = ['GET'])
 def wordpage(title):
     print("here2: ", title)
-    word = Words.query.filter(Words.title.like(title)).all()
+    word = Words.query.filter(Words.title.ilike(title)).first()
     html = render_template("wordpage.html", 
-        defs = word[0].definitions,
+        defs = word.definitions,
         title = title.capitalize())
         
     return make_response(html)
@@ -71,27 +76,56 @@ def uploadword():
     print("here3")
     form = WordForm()
     if form.validate_on_submit():
-        db.session.add(Words(title = form.word.data))
-        # make lowercase here
-        db.session.commit()
-        return redirect({{url_for('main.homepage')}})
-
+        try:
+            db.session.add(Words(title = form.word.data))
+            # make lowercase here
+            db.session.commit()
+            return redirect(url_for('main.wordpage', title=form.word.data))
+        except sqlalchemy.exc.IntegrityError as e:
+            # some popup with error message with Bootstrap
+            print(type(e))
+            raise
     return render_template('uploadword.html', form=form)
     # html = render_template("words.html", words = words)
     # return make_response(html)
 
-@main.route("/uploaddef", methods = ['GET', 'POST'])
-def uploaddef():
+@main.route("/<string:word>/uploaddef", methods = ['GET', 'POST'])
+def uploaddef(word):
     print("here3")
     form = DefinitionForm()
     if form.validate_on_submit():
-        db.session.add(Defs(definition = form.definition.data))
-        # word2.definitions.append(def21)
-        db.session.commit()
-
+        try:
+            definition = Defs(definition = form.definition.data)
+            wordobj = Words.query.filter(Words.title.ilike(word)).first()
+            db.session.add(definition)
+            wordobj.definitions.append(definition)
+            db.session.commit()
+            return redirect(url_for('main.wordpage', title=word))
+        except sqlalchemy.exc.IntegrityError as e:
+            # some popup with error message with Bootstrap
+            print(type(e))
+            raise
     return render_template('uploaddef.html', form=form)
 
-@main.route("/uploadsign", methods = ['GET', 'POST'])
+@main.route("/<string:word>/<int:defid>/edit", methods = ['GET', 'POST'])
+def editdef(word, defid):
+    print("here3")
+    form = EditDefinitionForm()
+    defobj = Defs.query.filter(Defs.id.like(defid)).first()
+    form.definition.data = defobj.definition
+    if form.validate_on_submit():
+        try:
+            definition = Defs(definition = form.definition.data)
+            db.session.update(Defs.definition, definition)
+            db.session.commit()
+            return redirect(url_for('main.wordpage', title=word))
+        except sqlalchemy.exc.IntegrityError as e:
+            # some popup with error message with Bootstrap
+            print(type(e))
+            raise
+    return render_template('uploaddef.html', form=form)
+
+@main.route("/<string:word/<int:defid>/uploadsign", methods = ['GET', 'POST'])
 def uploadsign():
     print("here3")
     form = DefinitionForm()
