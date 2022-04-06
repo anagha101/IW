0ee5f2d0bda46c67bfa3c99b518ca63cdd904530
Anagha Rajagopalan <68399871+anagha101@users.noreply.github.com>
2022-04-06 12:59:08 -0400

    edit working!

--- aslsearch/main/routes.py
+++ aslsearch/main/routes.py
@@ -3,11 +3,21 @@ from flask.blueprints import Blueprint
 from aslsearch.models import Words, Defs, Signs
 from aslsearch import db
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
 from wtforms.validators import URL
 import sqlalchemy
 import re
+from urllib.parse import urlparse
+
+def validate_url(form, field):
+        url_regex = r'''((http|https)\:\/\/)?www\.youtube\.com\/watch\?v=[\w]{11}'''
+        regex = re.compile(url_regex)
+        match = regex.match(field.data)
+        if not match:
+            print("inside error")
+            # HELP NOT RAISING
+            raise ValidationError('Field must be a Youtube URL')
 
 def convert_url(url):
     video_id = url[-11:]
@@ -22,26 +32,16 @@ class DefinitionForm(FlaskForm):
     definition = StringField('Definition', validators=[DataRequired()])
     submit = SubmitField('Add Definition')
 
-# class EditDefinitionForm(FlaskForm):
-#     definition = StringField('Definition', validators=[DataRequired()])
-#     submit = SubmitField('Add Definition')
+class EditDefinitionForm(FlaskForm):
+    definition = StringField('Definition', validators=[DataRequired()])
+    submit = SubmitField('Edit Definition')
 
 class SignForm(FlaskForm):
     gloss = StringField('ASL Gloss', validators=[DataRequired()])
     pos = StringField('Part of Speech', validators=[DataRequired()])
-    url = StringField('YouTube URL', validators=[DataRequired()])
-    context = TextAreaField('Context')
+    url = StringField('YouTube URL', validators=[DataRequired(), validate_url])
+    context = StringField('Context', validators=[DataRequired()])
     submit = SubmitField('Add Sign')
-
-    def validate_url(form, url):
-        pattern = re.compile("https://www\.youtube\.com/watch\?v=[\w]{11}")
-        match = pattern.match(url.data)
-        pattern = re.compile("www\.youtube\.com/watch\?v=[\w]{11}")
-        match = pattern.match(url.data)
-        print("correct?: ", match)
-        if not match:
-            print("inside error")
-            raise ValidationError('Field must be a Youtube URL')
 
 main = Blueprint('main', __name__)
 
@@ -123,32 +123,42 @@ def uploaddef(word):
             raise
     return render_template('uploaddef.html', word=word, form=form)
 
-# @main.route("/<string:word>/<int:defid>/edit", methods = ['GET', 'POST'])
-# def editdef(word, defid):
-#     print("here3")
-#     form = EditDefinitionForm()
-#     defobj = Defs.query.filter(Defs.id.like(defid)).first()
-#     form.definition.data = defobj.definition
-#     if form.validate_on_submit():
-#         try:
-#             definition = Defs(definition = form.definition.data)
-#             db.session.update(Defs.definition, definition)
-#             db.session.commit()
-#             return redirect(url_for('main.wordpage', title=word))
-#         except sqlalchemy.exc.IntegrityError as e:
-#             # some popup with error message with Bootstrap
-#             print(type(e))
-#             raise
-#     return render_template('uploaddef.html', form=form)
+@main.route("/<string:word>/<int:defid>/edit", methods = ['GET', 'POST'])
+def editdef(word, defid):
+    print("here3")
+    form = EditDefinitionForm()
+    defobj = Defs.query.get(defid)
+    if request.method == 'GET':
+        form.definition.data = defobj.definition
+    if form.validate_on_submit():
+        print("in submit")
+        try:
+            print("in try")
+            print("OG: ", defobj.definition)
+            print("new: ", form.definition.data)
+            defobj.definition = form.definition.data
+            print("replaced? :", defobj.definition)
+            db.session.commit()
+            return redirect(url_for('main.wordpage', title=word))
+        except sqlalchemy.exc.IntegrityError as e:
+            # some popup with error message with Bootstrap
+            print(type(e))
+            raise
+    return render_template('editdef.html', form=form)
 
 @main.route("/<string:word>/<int:defid>/uploadsign", methods = ['GET', 'POST'])
 def uploadsign(word, defid):
     print("here3")
     form = SignForm()
     defobj = Defs.query.get(defid)
+    print("here4")
     if form.validate_on_submit():
+        print("inside submit")
         try:
+            print("inside try")
+            print(form.url.data)
             embed_url = convert_url(form.url.data)
+            print(embed_url)
             sign = Signs(gloss = form.gloss.data, pos = form.pos.data, context = form.context.data, url = embed_url)
             db.session.add(sign)
             defobj.signs.append(sign)
