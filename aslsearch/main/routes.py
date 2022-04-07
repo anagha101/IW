5dba7391d049b0cb5f620dea7eecf79428536788
Anagha Rajagopalan <68399871+anagha101@users.noreply.github.com>
2022-04-06 23:32:37 -0400

    finished upload and login!!!

--- aslsearch/main/routes.py
+++ aslsearch/main/routes.py
@@ -1,4 +1,4 @@
-from flask import request, make_response, render_template, redirect, url_for
+from flask import request, session, make_response, render_template, redirect, url_for
 from flask.blueprints import Blueprint
 from aslsearch.models import Words, Defs, Signs
 from aslsearch import db
@@ -8,7 +8,20 @@ from wtforms.validators import DataRequired, ValidationError
 from wtforms.validators import URL
 import sqlalchemy
 import re
-from urllib.parse import urlparse
+from cas import CASClient
+
+cas_client = CASClient(
+    version=3,
+    service_url='http://localhost:5000/login?next=%2F',
+    server_url='https://fed.princeton.edu/cas/login'
+)
+
+def is_admin():
+    if 'admin' in session:
+        isAdmin = True
+    else:
+        isAdmin = False
+    return isAdmin
 
 def validate_url(form, field):
         url_regex = r'''((http|https)\:\/\/)?www\.youtube\.com\/watch\?v=[\w]{11}'''
@@ -32,29 +45,28 @@ class DefinitionForm(FlaskForm):
     definition = StringField('Definition', validators=[DataRequired()])
     submit = SubmitField('Add Definition')
 
-class EditDefinitionForm(FlaskForm):
-    definition = StringField('Definition', validators=[DataRequired()])
-    submit = SubmitField('Edit Definition')
-
 class SignForm(FlaskForm):
     gloss = StringField('ASL Gloss', validators=[DataRequired()])
     pos = StringField('Part of Speech', validators=[DataRequired()])
-    url = StringField('YouTube URL', validators=[DataRequired(), validate_url])
+    url = StringField('YouTube URL', validators=[DataRequired(), URL()])
     context = StringField('Context', validators=[DataRequired()])
     submit = SubmitField('Add Sign')
+
+class DeleteForm(FlaskForm):
+    idk = StringField('idk')
 
 main = Blueprint('main', __name__)
 
 @main.route("/", methods=['GET'])
 def homepage():
-    html = render_template('index.html')
+    html = render_template('index.html', admin=is_admin())
     response = make_response(html)
     return response
 
 
 @main.route("/about", methods=['GET'])
 def about():
-    html = render_template('about.html')
+    html = render_template('about.html', admin=is_admin())
     response = make_response(html)
     return response
 
@@ -72,7 +84,7 @@ def words():
         words = Words.query.filter(Words.title.like(keyword)).order_by(Words.title).all()
         print(words)
 
-    html = render_template("words.html", words = words)
+    html = render_template("words.html", words = words, admin=is_admin())
 
     return make_response(html)
 
@@ -82,7 +94,8 @@ def wordpage(title):
     word = Words.query.filter(Words.title.ilike(title)).first()
     html = render_template("wordpage.html", 
         defs = word.definitions,
-        title = title.capitalize())
+        title = title.capitalize(),
+        admin=is_admin())
         
     return make_response(html)
 
@@ -100,7 +113,7 @@ def uploadword():
             # some popup with error message with Bootstrap
             print(type(e))
             raise
-    return render_template('uploadword.html', form=form)
+    return render_template('uploadword.html', form=form, admin=is_admin())
     # html = render_template("words.html", words = words)
     # return make_response(html)
 
@@ -121,37 +134,47 @@ def uploaddef(word):
             # definition already exists
             print(type(e))
             raise
-    return render_template('uploaddef.html', word=word, form=form)
+    return render_template('uploaddef.html', word=word, form=form, admin=is_admin())
 
 @main.route("/<string:word>/<int:defid>/edit", methods = ['GET', 'POST'])
 def editdef(word, defid):
     print("here3")
-    form = EditDefinitionForm()
+    form = DefinitionForm()
     defobj = Defs.query.get(defid)
     if request.method == 'GET':
         form.definition.data = defobj.definition
     if form.validate_on_submit():
-        print("in submit")
         try:
-            print("in try")
-            print("OG: ", defobj.definition)
-            print("new: ", form.definition.data)
             defobj.definition = form.definition.data
-            print("replaced? :", defobj.definition)
             db.session.commit()
             return redirect(url_for('main.wordpage', title=word))
         except sqlalchemy.exc.IntegrityError as e:
             # some popup with error message with Bootstrap
             print(type(e))
             raise
-    return render_template('editdef.html', form=form)
+    return render_template('uploaddef.html', form=form, admin=is_admin())
+
+@main.route("/<string:word>/<int:defid>/delete", methods = ['GET', 'POST'])
+def deletedef(word, defid):
+    print("inside delete fn")
+    defobj = Defs.query.get(defid)
+    try:
+        print("inside try")
+        for sign in defobj.signs:
+            db.session.delete(sign)
+        db.session.delete(defobj)
+        db.session.commit()
+        return redirect(url_for('main.wordpage', title=word))
+    except sqlalchemy.exc.IntegrityError as e:
+        # some popup with error message with Bootstrap
+        print(type(e))
+        raise
 
 @main.route("/<string:word>/<int:defid>/uploadsign", methods = ['GET', 'POST'])
 def uploadsign(word, defid):
     print("here3")
     form = SignForm()
     defobj = Defs.query.get(defid)
-    print("here4")
     if form.validate_on_submit():
         print("inside submit")
         try:
@@ -169,5 +192,86 @@ def uploadsign(word, defid):
             # sign already exists!
             print(type(e))
             raise
+    return render_template('uploadsign.html', 
+        word=word, 
+        definition=defobj.definition, 
+        form=form,
+        admin=is_admin())
 
-    return render_template('uploadsign.html', word=word, definition=defobj.definition, form=form)
+@main.route("/<string:word>/<int:defid>/<int:signid>/edit", methods = ['GET', 'POST'])
+def editsign(word, defid, signid):
+    print("here3")
+    form = SignForm()
+    signobj = Signs.query.get(signid)
+    if request.method == 'GET':
+        form.gloss.data = signobj.gloss
+        form.pos.data = signobj.pos
+        form.url.data = signobj.url
+        form.context.data = signobj.context
+    if form.validate_on_submit():
+        print("in submit")
+        try:
+            signobj.gloss = form.gloss.data
+            signobj.pos = form.pos.data
+            signobj.url = form.url.data
+            signobj.context = form.context.data
+            db.session.commit()
+            return redirect(url_for('main.wordpage', title=word))
+        except sqlalchemy.exc.IntegrityError as e:
+            # some popup with error message with Bootstrap
+            print(type(e))
+            raise
+    return render_template('uploadsign.html', form=form, admin=is_admin())
+
+@main.route("/<string:word>/<int:defid>/<int:signid>/delete", methods = ['GET', 'POST'])
+def deletesign(word, defid, signid):
+    signobj = Signs.query.get(signid)
+    try:
+        db.session.delete(signobj)
+        db.session.commit()
+        return redirect(url_for('main.wordpage', title=word))
+    except sqlalchemy.exc.IntegrityError as e:
+        # some popup with error message with Bootstrap
+        print(type(e))
+        raise
+
+@main.route('/login')
+def login():
+    print("inside login")
+    if 'admin' in session:
+        print("Username exists:", session['admin'])
+        # Already logged in
+        return redirect(url_for('main.homepage'))
+
+    next = request.args.get('next')
+    ticket = request.args.get('ticket')
+    if not ticket:
+        # Send to CAS login
+        cas_login_url = cas_client.get_login_url()
+        return redirect(cas_login_url)
+
+    user, attributes, pgtiou = cas_client.verify_ticket(ticket)
+
+    if not user:
+        return 'Failed to verify ticket. <a href="/login">Login</a>'
+    else:  # Login success, redirect
+        if (user=='anaghar' or user=='nadb') :
+            session['admin'] = True
+        else:
+            session['admin'] = False
+        return redirect(next)
+
+
+@main.route('/logout')
+def logout():
+    redirect_url = url_for('main.logout_callback', _external=True)
+    cas_logout_url = cas_client.get_logout_url(redirect_url)
+
+    return redirect(cas_logout_url)
+
+
+@main.route('/logout_callback')
+def logout_callback():
+    # Logout success, redirect
+    session.pop('admin', None)
+    return redirect(url_for('main.homepage'))
