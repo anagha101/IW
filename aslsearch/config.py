2c945259672ff6205a4794a85ba3bcdbb758dba6
Anagha Rajagopalan <68399871+anagha101@users.noreply.github.com>
2022-04-21 14:28:46 -0400

    right db

--- aslsearch/config.py
+++ aslsearch/config.py
@@ -10,7 +10,7 @@ class Config:
         # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
         # SQLALCHEMY_DATABASE_URI = 'postgres://zxcztxnsiaqmcs:bba59de47e28686189bbff3accfaa503cf44e979cb27c1f8a3b5e8d2bc8dbeff@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d5c55gh1c6l45j'
     else:
-        SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:{password}@localhost/aslsearch'
+        SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:{password}@localhost/aslsearchdb'
         # SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_DB_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = 'development'
