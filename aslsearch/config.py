268ac7c7899ae08d937236a5891b6046678b6a3c
Anagha Rajagopalan <68399871+anagha101@users.noreply.github.com>
2022-04-25 18:01:36 -0400

    local config

--- aslsearch/config.py
+++ aslsearch/config.py
@@ -9,8 +9,8 @@ password = os.environ.get('DB_PASSWORD')
 
 class Config:
     SECRET_KEY = 'asl_search_iw_app'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
-    # SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:{password}@localhost/aslsearchdb'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:{password}@localhost/aslsearchdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = 'development'
     DEBUG = True
